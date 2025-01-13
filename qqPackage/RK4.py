aa7d81c4517e932beb88ac2e14fce47a60a2fd48
MraDmr0 <dimaremario24@gmail.com>
2025-01-13 19:29:56 +0100

    Update RK4.py

--- qqPackage/RK4.py
+++ qqPackage/RK4.py
@@ -64,32 +64,33 @@ def rk4_qb(psi0 , t0 , tf , N , S , wr , wl , w , F ):
 
 @njit
 def rk4_qq(psi0 , t0 , tf , N , S , wr , wl , w , F ):
-
+    #definition of time array
     dt = (tf-t0)/N
     t = np.linspace(t0 , tf , N+1)
-    
+    #assignement of RK coefficients
     K0 =  np.zeros(4 , dtype = complex128)
     K1 =  np.zeros(4 , dtype = complex128)
     K2 =  np.zeros(4 , dtype = complex128)
     K3 =  np.zeros(4 , dtype = complex128)
-    
+    #check if the number of points saved on the output file
     if N%S != 0 :
         a     = int(N/S)+2
     else:
         a     = int(N/S)+1
+    #assign variables to store output data
     tff    = np.zeros(a)
     psif0  = np.zeros(a , dtype = complex128)
     psif1  = np.zeros(a , dtype = complex128)
     psif2  = np.zeros(a , dtype = complex128)
     psif3  = np.zeros(a , dtype = complex128)
-    
+    #load the initial condition on output data
     psif0[0] = psi0[0]
     psif1[0] = psi0[1]
     psif2[0] = psi0[2]
     psif3[0] = psi0[3]
     tff[0]   = t[0]
     i        = 1
-
+    #RK4 cycle
     for j in range(1 , N+1):
     
         V  = potential_qq(t[j-1] , dt , wr , wl , w , F )
@@ -100,11 +101,10 @@ def rk4_qq(psi0 , t0 , tf , N , S , wr , wl , w , F ):
         K3 = np.dot(V[2] , psi0 + dt*K2)
 
         psi0 = psi0 + dt/6*(K0 + 2*K1 + 2*K2 + K3)
-
+        #renormalizaton of computed solutio
         norm   = np.sqrt(np.dot(psi0.conjugate() , psi0))
         psi0   = psi0/norm
-
-       
+        #append to output data
         if j%S == 0 or j == N:
 
             psif0[i] = psi0[0]
