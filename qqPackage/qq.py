d75402521d5cd4d6859f6b76db89ca1bcc3dc4c8
MraDmr0 <dimaremario24@gmail.com>
2025-01-24 19:50:36 +0100

    Update qq.py

--- qqPackage/qq.py
+++ qqPackage/qq.py
@@ -7,6 +7,7 @@ from Read_input import read_run
 from Write_output import write_out_qb , write_outt , save_out , write_out_qq
 from RK4 import rk4_qb , rk4_qq
 from Plot import plot_qb , plot_qq
+from Envelope import envelope
 import sys
 #imaginary unit
 im = 1j
@@ -15,25 +16,26 @@ print("Execution of qq.py started\n")
 #input file reading
 filein = str(sys.argv[1])
 print("Reading input file "+str(filein)+"...\n")
-prefix , D , ti , tf , N, S , psi , wr , wl , w , F = read_run(filein)
+prefix , D , ti , tf , N, S , psi , wr , wl , w , mode , F , t0 , t1 , sigma , t00 , t11 = read_run(filein)
 #
 #qubit run
 if D == 2:
     print("Begin of calculation...")
     #output
-    write_out_qb(D , ti , tf , N,S , psi , wr , wl , w , F)
+    write_out_qb(D , ti , tf , N, S , psi , wr , wl , w , F)
     #
     time1 = time.time()
     #
     #time evolution    
-    psiff , tff = rk4_qb( psi , ti , tf , N, S , wr , wl , w , F)
+    E = envelope(ti , tf , N , mode , F , t0 , t1 , sigma , t00 , t11)
+    psiff , tff , Eout = rk4_qb(psi , ti , tf , N , S , wr , wl , w , E)
     #
     time2 = time.time()
     save_out(prefix , psiff , tff)
     #
     #output
     write_outt(prefix, time1 , time2)
-    plot_qb(prefix)
+    plot_qb(prefix , Eout = Eout)
 elif D == 4:
     print("Begin of calculation...")
     #output
@@ -41,16 +43,15 @@ elif D == 4:
     #
     time1 = time.time()
     #
-    #time evolution    
-    psiff , tff = rk4_qq( psi , ti , tf , N, S , wr , wl , w , F)
+    #time evolution   
+    E = envelope(ti , tf , N , mode , F , t0 , t1 , sigma , t00 , t11) 
+    psiff , tff , Eout = rk4_qq(psi , ti , tf , N , S , wr , wl , w , E)
     time2 = time.time()
     save_out(prefix , psiff , tff)
     #
     #output
     write_outt(prefix, time1 , time2)
-    plot_qq(prefix)
-
-
+    plot_qq(prefix , Eout = Eout)
 
 else:
     raise TypeError("Wrong number of states")
