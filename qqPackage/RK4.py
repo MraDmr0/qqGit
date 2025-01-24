c4da86cbee1c40afb10dd245183964a819065caa
MraDmr0 <dimaremario24@gmail.com>
2025-01-24 19:48:21 +0100

    Update RK4.py

--- qqPackage/RK4.py
+++ qqPackage/RK4.py
@@ -5,7 +5,7 @@ from numba import njit , complex64 , complex128
 im = 1j
 
 @njit
-def rk4_qb(psi0 , t0 , tf , N , S , wr , wl , w , F ):
+def rk4_qb(psi0 , t0 , tf , N , S , wr , wl , w , E ):
     """Function that performs the time evolution with RK4 method usign potential_qb"""
    #conversion of Rabi frequencies in the form accepted by potential_qb
     wrr = np.zeros(5 , dtype = complex64)
@@ -31,15 +31,17 @@ def rk4_qb(psi0 , t0 , tf , N , S , wr , wl , w , F ):
     tff    = np.zeros(a)
     psif0  = np.zeros(a , dtype = complex128)
     psif1  = np.zeros(a , dtype = complex128)
+    Eout   = np.zeros(a)
     #load the initial condition on output data
     psif0[0] = psi0[0]
     psif1[0] = psi0[1]
     tff[0]   = t[0]
+    Eout[0]  = E[0]
     i        = 1
    #RK4 cycle
     for j in range(1 , N+1):
     
-        V  = potential_qb(t[j-1] , dt , wrr, w , F )
+        V  = potential_qb(t[j-1] , dt , wrr, w , E[j-1] )
         
         K0 = np.dot(V[0] , psi0)
         K1 = np.dot(V[1] , psi0 + 0.5*dt*K0)
@@ -57,16 +59,18 @@ def rk4_qb(psi0 , t0 , tf , N , S , wr , wl , w , F ):
             psif0[i] = psi0[0]
             psif1[i] = psi0[1]
             tff[i]   = t[j]
+            Eout[i]  = E[j]
             i = i + 1
    
     psiff = np.column_stack((psif0 , psif1))
-    return psiff , tff
+    return psiff , tff , Eout
 
 @njit
-def rk4_qq(psi0 , t0 , tf , N , S , wr , wl , w , F ):
+def rk4_qq(psi0 , t0 , tf , N , S , wr , wl , w , E ):
     #definition of time array
     dt = (tf-t0)/N
     t = np.linspace(t0 , tf , N+1)
+    
     #assignement of RK coefficients
     K0 =  np.zeros(4 , dtype = complex128)
     K1 =  np.zeros(4 , dtype = complex128)
@@ -83,17 +87,19 @@ def rk4_qq(psi0 , t0 , tf , N , S , wr , wl , w , F ):
     psif1  = np.zeros(a , dtype = complex128)
     psif2  = np.zeros(a , dtype = complex128)
     psif3  = np.zeros(a , dtype = complex128)
+    Eout   = np.zeros(a)
     #load the initial condition on output data
     psif0[0] = psi0[0]
     psif1[0] = psi0[1]
     psif2[0] = psi0[2]
     psif3[0] = psi0[3]
     tff[0]   = t[0]
+    Eout[0]  = E[0]
     i        = 1
     #RK4 cycle
     for j in range(1 , N+1):
     
-        V  = potential_qq(t[j-1] , dt , wr , wl , w , F )
+        V  = potential_qq(t[j-1] , dt , wr , wl , w , E[j-1] )
         
         K0 = np.dot(V[0] , psi0)
         K1 = np.dot(V[1] , psi0 + 0.5*dt*K0)
@@ -112,8 +118,8 @@ def rk4_qq(psi0 , t0 , tf , N , S , wr , wl , w , F ):
             psif2[i] = psi0[2]
             psif3[i] = psi0[3]
             tff[i]   = t[j]
+            Eout[i]  = E[j]
             i = i + 1
    
     psiff = np.column_stack((psif0 , psif1 , psif2 , psif3))
-    return psiff , tff
-
+    return psiff , tff , Eout
